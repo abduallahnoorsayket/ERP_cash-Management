34ad5d7fe969ffefb3632696ef7d99a0f1d5891d
abduallahnoorsayket <abdullahnoorsayket@gmail.com>
2021-11-16 22:18:43 +0600

    permission ui added

--- src/components/pages/modules/base/group/GroupCreate.vue
+++ src/components/pages/modules/base/group/GroupCreate.vue
@@ -21,7 +21,235 @@
 
                  <div class="form-group">
                   <label>User Permissions</label>
-                  <input
+                  <div class="p-listbox p-component">
+                    <!-- <div class="p-listbox-header">
+                      <div class="p-listbox-filter-container">
+                        <input
+                          type="text"
+                          class="p-listbox-filter p-inputtext p-component"
+                          v-model="searchQuery"
+                          placeholder="Filter"
+                        />
+                        <span class="p-listbox-filter-icon pi pi-search"></span>
+                      </div>
+                    </div> -->
+                    <div class="p-listbox-list-wrapper">
+                      <div
+                        role="listbox"
+                        aria-multiselectable="multiple"
+                        class="p-listbox-list permission-custom-height"
+                      >
+                        <div
+                          class="module-permission-section"
+                        >
+                          <div class="main-card lastActivityCard">
+                            <h5 class="listTitle">Base Module</h5>
+                          </div>
+                          <div class="row module">
+                            <div class="col-md-12">
+                              <div class="p-listbox-item">
+                                <label class="form-check-label">
+                                  <input
+                                    type="checkbox"
+                                    class="form-check-input"
+                                  />
+                                  <span class="checkmark"></span>
+                                    Base Module
+                                </label>
+                              </div>
+                            </div>
+                          </div>
+                           <div class="row submodule">
+                              <div class="col-md-12">
+                                <div class="p-listbox-item">
+                                  <label
+                                    class="form-check-label"
+                                    style="font-weight: 600"
+                                  >
+                                    <input
+                                      type="checkbox"
+                                      class="form-check-input"
+                                    />
+                                    <span class="checkmark"></span>
+                                    User Management
+                                  </label>
+                                </div>
+                              </div>
+                            </div>
+                            <div class="row permission_name">
+                              <div
+                                class="col-sm-6 col-md-6 col-xl-3"
+                              >
+                                <div class="p-listbox-item">
+                                  <label
+                                    class="form-check-label"
+                                    style="font-weight: 300"
+                                  >
+                                    <input
+                                      type="checkbox"
+                                      class="form-check-input"
+                                    />
+                                    <span class="checkmark"></span>
+                                    User Add
+                                  </label>
+                                </div>
+                                
+                              </div>
+                              <div
+                                class="col-sm-6 col-md-6 col-xl-3"
+                              >
+                                <div class="p-listbox-item">
+                                  <label
+                                    class="form-check-label"
+                                    style="font-weight: 300"
+                                  >
+                                    <input
+                                      type="checkbox"
+                                      class="form-check-input"
+                                    />
+                                    <span class="checkmark"></span>
+                                    User Edit
+                                  </label>
+                                </div>
+                                
+                              </div>
+                              <div
+                                class="col-sm-6 col-md-6 col-xl-3"
+                              >
+                                <div class="p-listbox-item">
+                                  <label
+                                    class="form-check-label"
+                                    style="font-weight: 300"
+                                  >
+                                    <input
+                                      type="checkbox"
+                                      class="form-check-input"
+                                    />
+                                    <span class="checkmark"></span>
+                                    User Delete
+                                  </label>
+                                </div>
+                                
+                              </div>
+                              <div
+                                class="col-sm-6 col-md-6 col-xl-3"
+                              >
+                                <div class="p-listbox-item">
+                                  <label
+                                    class="form-check-label"
+                                    style="font-weight: 300"
+                                  >
+                                    <input
+                                      type="checkbox"
+                                      class="form-check-input"
+                                    />
+                                    <span class="checkmark"></span>
+                                    User View
+                                  </label>
+                                </div>
+                                
+                              </div>
+                            </div>
+                            <div class="row submodule">
+                              <div class="col-md-12">
+                                <div class="p-listbox-item">
+                                  <label
+                                    class="form-check-label"
+                                    style="font-weight: 600"
+                                  >
+                                    <input
+                                      type="checkbox"
+                                      class="form-check-input"
+                                    />
+                                    <span class="checkmark"></span>
+                                    Group Management
+                                  </label>
+                                </div>
+                              </div>
+                            </div>
+                            <div class="row permission_name">
+                              <div
+                                class="col-sm-6 col-md-6 col-xl-3"
+                              >
+                                <div class="p-listbox-item">
+                                  <label
+                                    class="form-check-label"
+                                    style="font-weight: 300"
+                                  >
+                                    <input
+                                      type="checkbox"
+                                      class="form-check-input"
+                                    />
+                                    <span class="checkmark"></span>
+                                    Group Add
+                                  </label>
+                                </div>
+                                
+                              </div>
+                              <div
+                                class="col-sm-6 col-md-6 col-xl-3"
+                              >
+                                <div class="p-listbox-item">
+                                  <label
+                                    class="form-check-label"
+                                    style="font-weight: 300"
+                                  >
+                                    <input
+                                      type="checkbox"
+                                      class="form-check-input"
+                                    />
+                                    <span class="checkmark"></span>
+                                    Group Edit
+                                  </label>
+                                </div>
+                                
+                              </div>
+                              <div
+                                class="col-sm-6 col-md-6 col-xl-3"
+                              >
+                                <div class="p-listbox-item">
+                                  <label
+                                    class="form-check-label"
+                                    style="font-weight: 300"
+                                  >
+                                    <input
+                                      type="checkbox"
+                                      class="form-check-input"
+                                    />
+                                    <span class="checkmark"></span>
+                                    Group Delete
+                                  </label>
+                                </div>
+                                
+                              </div>
+                              <div
+                                class="col-sm-6 col-md-6 col-xl-3"
+                              >
+                                <div class="p-listbox-item">
+                                  <label
+                                    class="form-check-label"
+                                    style="font-weight: 300"
+                                  >
+                                    <input
+                                      type="checkbox"
+                                      class="form-check-input"
+                                    />
+                                    <span class="checkmark"></span>
+                                    Group View
+                                  </label>
+                                </div>
+                                
+                              </div>
+                            </div>
+                            
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+
+
+
+                  <!-- <input
                     type="text"
                     class="form-control search-input"
                     autocomplete="off"
@@ -48,16 +276,21 @@
                       :error="errors.permissions"
                       v-if="errors"
                     />
-                  </div>
+                  </div> -->
+                
+                
+                
+                
                 </div>
 
               </div>
               <!-- end col -->
 
-              <div class="col-md-6">
+              <!-- <div class="col-md-6">
                
-              </div>
+              </div> -->
               <!-- end col -->
+             
 
               <div class="col-md-12">
                 <div class="form-group">
@@ -84,7 +317,7 @@ import axios from "@/axios";
 import Layout from "../Layout.vue";
 import PageTitle from "@/components/layouts/partials/PageTitle";
 import Swal from "sweetalert2";
-import ValidationError from "@/components/layouts/partials/ValidationError.vue";
+// import ValidationError from "@/components/layouts/partials/ValidationError.vue";
 // import permissions from "@/permisson";
 
 export default {
@@ -92,7 +325,7 @@ export default {
   components: {
     Layout,
     PageTitle,
-    ValidationError,
+    // ValidationError,
   },
   data() {
     return {
@@ -124,7 +357,7 @@ export default {
           Swal.fire({
             icon: "success",
             text: "You have successfully created a Group.",
-          }).then((result) => {
+          }).then(() => {
             this.$router.push("group-list");
           });
         })
@@ -163,5 +396,187 @@ export default {
 }
 .cus_check_padding label {
   padding-left: 8px;
+}
+/*  new permission UI */
+.p-listbox .p-listbox-list {
+  /* padding: 0.5rem 0; */
+  /* height: 485px; */
+  height: auto !important;
+  padding: 0.2rem 1.5rem;
+}
+.lastActivityCard {
+  /* background: #e6f7f4; */
+  background: #d8f3e5;
+  padding: 1px;
+  border-left: 2px solid #69c5a5;
+  border-radius: 1px;
+  margin: 0px 0px 15px 0px;
+}
+.listTitle {
+  margin-left: 10px;
+  margin-top: 10px;
+}
+.submodule {
+  margin-left: 40px;
+}
+.permission_name {
+  margin-left: 100px;
+  margin-bottom: 15px;
+}
+.module-permission-section {
+  margin: 15px 0px 30px 0px;
+}
+.module {
+  margin: 20px 0px 10px 10px;
+}
+.is_checked {
+  color: red;
+}
+/* new styles */
+.p-listbox {
+    background: #fff;
+    color: #495057;
+    border: 1px solid #ced4da;
+    border-radius: 3px;
+}
+/*  new list box style */
+
+.p-listbox {
+  background: #fff;
+  color: #495057;
+  border: 1px solid #ced4da;
+  border-radius: 3px;
+}
+.p-component {
+  font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Helvetica, Arial, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol;
+  font-size: 1rem;
+  font-weight: 400;
+}
+.p-listbox .p-listbox-header {
+  padding: 0.5rem 0.5rem;
+  border-bottom: 0 none;
+  color: #495057;
+  background: #f5f5f5;
+  margin: 0;
+  border-top-right-radius: 3px;
+  border-top-left-radius: 3px;
+}
+.p-component,
+.p-component * {
+  -webkit-box-sizing: border-box;
+  box-sizing: border-box;
+}
+.p-listbox-filter-container,
+.p-listbox-item {
+  position: relative;
+}
+
+.p-listbox .p-listbox-header .p-listbox-filter {
+  padding-right: 1.5rem;
+  width: 100%;
+  outline: none;
+}
+
+.p-fluid .p-inputtext {
+  width: 100%;
+}
+.p-inputtext {
+  margin: 0;
+}
+
+.p-inputtext {
+  font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Helvetica, Arial, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol;
+  font-size: 14px;
+  color: #495057;
+  background: #fff;
+  padding: 0.3rem 0.5rem;
+  border: 1px solid #ced4da;
+  -webkit-transition: background-color 0.2s, color 0.2s, border-color 0.2s, -webkit-box-shadow 0.2s;
+  transition: background-color 0.2s, color 0.2s, border-color 0.2s, -webkit-box-shadow 0.2s;
+  transition: background-color 0.2s, color 0.2s, border-color 0.2s, box-shadow 0.2s;
+  transition: background-color 0.2s, color 0.2s, border-color 0.2s, box-shadow 0.2s, -webkit-box-shadow 0.2s;
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  appearance: none;
+  border-radius: 3px;
+}
+
+.p-listbox .p-listbox-header .p-listbox-filter-icon {
+  right: 0.5rem;
+  color: #6c757d;
+}
+
+
+.p-link,
+.pi {
+  font-size: 1rem;
+}
+.p-listbox-filter-icon {
+  position: absolute;
+  top: 50%;
+  margin-top: -0.5rem;
+}
+
+.pi-search:before {
+  content: "\E908";
+}
+
+.pi:before {
+  --webkit-backface-visibility: hidden;
+  -webkit-backface-visibility: hidden;
+  backface-visibility: hidden;
+}
+.p-listbox-list-wrapper {
+  overflow: auto;
+}
+/* .p-listbox .p-listbox-list {
+  padding: 0.5rem 0;
+} */
+
+.p-listbox-list {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+}
+
+.p-listbox .p-listbox-list .p-listbox-item {
+  margin: 0;
+  padding: 0.1rem 1rem;
+  border: 0 none;
+  color: #495057;
+  -webkit-transition: -webkit-box-shadow 0.2s;
+  transition: -webkit-box-shadow 0.2s;
+  transition: box-shadow 0.2s;
+  transition: box-shadow 0.2s, -webkit-box-shadow 0.2s;
+  border-radius: 0;
+  font-size: 13px;
+  text-transform: capitalize;
+}
+.p-ink {
+  display: block;
+  position: absolute;
+  background: hsla(0, 0%, 100%, 0.5);
+  border-radius: 100%;
+  -webkit-transform: scale(0);
+  transform: scale(0);
+}
+
+.p-listbox .p-listbox-header .p-listbox-filter-icon {
+  right: 0.5rem;
+  color: #6c757d;
+}
+
+
+
+.p-link,
+.pi {
+  font-size: 1rem;
+}
+
+
+.p-listbox .p-listbox-list {
+  /* padding: 0.5rem 0; */
+  height: 185px;
+  padding: 0.2rem 1rem;
 }
 </style>
